7f76b2941fe9754af85c57ca75df7e40aee2db52
Dianalsl <djlu0222@gmail.com>
2025-02-14 14:05:30 -0500

    sloved lesson 3

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,11 @@
 # Lesson 3
+#Squares Solution
+
+#input
+tiles = input("Enter the number of tiles:") #input() is string
+tiles = int(tiles) #convert to integer
+#square root is just the exponent of 0.5
+calculation = int((tiles ** 0.5)//1) #square root
+
+# check whole number
+print(f"the maximun side length is: {calculation}")
