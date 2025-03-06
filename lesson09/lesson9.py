dc3348cab9fa9ce72adffa20f2e9b39c95600958
Dianalsl <djlu0222@gmail.com>
2025-03-06 14:25:03 -0500

    problem solved

--- lesson09/lesson9.py
+++ lesson09/lesson9.py
@@ -1,1 +1,34 @@
 # Lesson 9
+#import statment
+from random import choice
+
+asking = True
+player = " "
+
+while asking:
+    player = str(input("Enrter a choice of (rock , paper, scissors): "))
+
+    if player in {'rock','paper','scissors'}:
+        # we use in operator in python often with sets because of its faster execution speed
+        asking = False
+#end of while loop
+
+ai = choice(['rock', 'paper','scissors'])
+
+if player == ai:
+    print("Tie Game")
+elif player == 'rock':
+    if ai == "scissors":
+        print(f"You won, AI choose {ai}")
+    elif ai == "paper":
+        print(f"You lose, AI choose {ai}")
+elif player =="paper":
+    if ai == "rock":
+        print(f"You won, AI choose {ai}")
+    elif ai == "scissors":
+        print(f"You lose, AI choose {ai}")
+elif player =="rock":
+    if ai == "scissors":
+        print(f"You won, AI choose {ai}")
+    elif ai == "paper":
+        print(f"You lose, AI choose {ai}")
