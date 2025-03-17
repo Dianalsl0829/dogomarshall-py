9b8852a6df5b0d2c061b32d52dbdff955040b2e3
Dianalsl <djlu0222@gmail.com>
2025-03-17 14:37:34 -0400

    problem solved

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,12 @@
 # Lesson 10
+telephone =input()
+if len(telephone) == 4:
+    if (telephone[0] == '8' or telephone[0] == '9') and (telephone[3] == '8' or telephone[3] == '9'):
+        if (telephone[1] == telephone[2]):
+            print("telemarketer")
+        else:
+            print("not telemarketer")
+    else:
+        print("not telemarketer")
+else:
+    print("wrong number")
