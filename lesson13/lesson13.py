c3c1b55dea7d3e817bd43ff754d9c46627733f73
Dianalsl <djlu0222@gmail.com>
2025-03-19 13:18:54 -0400

    problem solved

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,16 @@
 # Lesson 13
+#input
+month = int(input())
+day = int(input())
+if month == 2 and day == 18:
+    print("This is a specaial day!")
+else:
+    if month < 2:
+        print("Before")
+    elif month > 2:
+        print("After")
+    else:
+        if day>18:
+            print("After")
+        elif day<18:
+            print("Before")
