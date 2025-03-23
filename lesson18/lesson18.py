f99d552d08d511dd9d700b86aab5bf4da15d9b67
Dianalsl <djlu0222@gmail.com>
2025-03-23 00:36:06 -0400

    added a working integer input

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,1 +1,8 @@
 # Lesson 18
+# num = int(input())
+# ans = []
+# for i in range(1, num+1):
+#     if num % i == 0 :
+#         ans.append(i)
+# print(f"The factors of {num} are {ans}")
+num = int(input("Enter an integer greater than 0:"))
