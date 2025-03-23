20c9bbf971ab13c71505be4f535957f265881b5b
Dianalsl <djlu0222@gmail.com>
2025-03-23 17:59:44 -0400

    problem sloved

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,19 @@
 # Lesson 20
+#solution
+'''
+set 10000 as the max value
+set an answer value
+check each number if it is a perfect number
+by cheking the sum of it's factor equals to itself
+if perfect, add to the asnwer
+'''
+ans = 0
+
+for num in range(1, 10001):
+    tem = 0
+    for i in range(1, num):
+        if num % i == 0:
+            tem += i
+        if tem==num:
+            ans += num
+print(f'the sum of all perfect number is {ans}.')
