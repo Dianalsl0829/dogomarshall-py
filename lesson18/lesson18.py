d8709589cf367478d222fde3a33634921d6fa617
Dianalsl <djlu0222@gmail.com>
2025-03-23 00:50:45 -0400

    added a for loop

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -6,3 +6,6 @@
 #         ans.append(i)
 # print(f"The factors of {num} are {ans}")
 num = int(input("Enter an integer greater than 0:"))
+
+for divider in range(1, mun+1):
+    print(f'divider variable is :{divider}')
