a18863904e732d3d67e145c8d9d2bbc43faa265e
Dianalsl <djlu0222@gmail.com>
2025-03-22 23:49:04 -0400

    problem solved

--- lesson17/lesson17.py
+++ lesson17/lesson17.py
@@ -1,1 +1,19 @@
 # Lesson 17
+# num = int(input())
+# ans = 1
+# if num!=0:
+#     for i in range(num,0,-1):
+#         ans *= i
+# else:
+#     ans = 1
+# print(ans)
+
+num = int(input())
+total = 1
+multiplier = 1
+
+while num>=multiplier:
+    total *= multiplier
+    multiplier +=1
+print(total)
+
