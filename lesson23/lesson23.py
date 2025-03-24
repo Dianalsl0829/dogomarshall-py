02a4da6caac0c3009ac97afe55e5f92d51b319d9
Dianalsl <djlu0222@gmail.com>
2025-03-24 14:41:03 -0400

    problem solved

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,17 @@
 # Lesson 23
+user = True
+total = 0
+average = 0
+while user:
+    state = input("Enter mark or 'exit' to quit:")
+    mark = 0
+    if state.lower() =='exit':
+        user = False
+        break
+    else:
+        if 0<=int(state)<=100:
+            mark = int(state)
+            total += mark
+            average += 1
+
+print(f'The average mark is {total/average}.')
