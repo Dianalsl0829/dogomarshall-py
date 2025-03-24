3f557fb021dd7a0785a0ccc3da0b6752e1f01cfd
Dianalsl <djlu0222@gmail.com>
2025-03-24 14:53:17 -0400

    problem sloved

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,14 @@
 # Lesson 24
+user = True
+length = 0
+longgest = ''
+while user:
+    name = input("Enter a name or 'X' to exit: ")
+    if name.lower() == "x":
+        user  = False
+        break
+    else:
+        if len(name)>length:
+            length = len(name)
+            longgest = name
+print(f"The longgest name is: {longgest}")
