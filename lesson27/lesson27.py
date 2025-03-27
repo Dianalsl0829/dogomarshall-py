c9fde5e74a7cf7151b9269ad9fad7acb33d5d01c
Dianalsl <djlu0222@gmail.com>
2025-03-27 08:50:27 -0400

    problem sloved

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,22 @@
 # Lesson 27
+#string cleaning function
+# Write a function that removes non-alphabetic characters from the given string. 
+# returns it as a lowercase version of the cleand string.
+
+def restring(string):
+    '''
+    make the string with only alfabetic characters and lower case
+    Args:
+    string: string
+    returns:
+    string
+    '''
+    cleared = ''
+    for i in string:
+        if i.isalpha():
+            cleared += i.lower()
+        
+    return cleared
+
+user = input("")
+print(restring(user))
