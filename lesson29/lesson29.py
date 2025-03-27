73f4b475065697057bccefc4e2f50759c03294b2
Dianalsl <djlu0222@gmail.com>
2025-03-27 10:34:12 -0400

    Problem sloved

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,51 @@
 # Lesson 29
+# giving a string, determind the amount of consonants.
+
+# def consonants(text):
+#     '''
+#     create a value and count for the consonants in the text
+#     Args: text: string
+#     return: integer as the value of the amount of consonants in the text.
+#     '''
+#     count  = 0
+#     lowcas = text.lower()
+#     if not text:
+#         return count
+#     elif len(text) == 1:
+#         if lowcas != "a" and lowcas !="e" and lowcas !="i" and lowcas !="o" and lowcas !="u":
+#             count += 1
+#         return count
+#     else:
+#         for i in lowcas:
+#             if i != "a" and i !="e" and i !="i" and i !="o" and i !="u":
+#                 count += 1
+#         return count
+# can't makesure it is alphavetical character
+
+def consonants_count(text, vowel = False):
+    '''
+    rturns the number of consonants in a given string
+    Args: 
+    text: a string value
+    return:
+    an interger
+    '''
+    ctr = 0
+    for character in text:
+        character = character.lower()
+        if character.isalpha() and character not in {'a','e','i','o','u'}:
+            ctr += 1
+
+    if not vowel:   
+        return ctr
+    else:
+        vow = 0
+        for character in text:
+            character = character.lower()
+            if character.isalpha() and character in {'a','e','i','o','u'}
+            vow += 1
+        return vow
+
+word = input()
+print(consonants(word))
+    
