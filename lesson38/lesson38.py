aa6c998601c1d75758513f46d6239ab5f3cdf4d1
Dianalsl <djlu0222@gmail.com>
2025-03-28 06:13:03 -0400

    problem solved

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,14 @@
 # Lesson 38
+#Palindromic Number
+def palindrome(text):
+    return text == text[::-1]
+
+palindromes =[]
+for i in range(999,99,-1):
+    for j in range(999,99,-1):
+        num = i*j
+        if palindrome(str(num)):
+            palindromes.append(num)
+
+print(f'the largest palindrome number made from two 3-digit number is {max(palindromes)}')
+
