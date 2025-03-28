9632d02af110c0b61212c1e22840041dccb6b5d4
Dianalsl <djlu0222@gmail.com>
2025-03-28 06:42:31 -0400

    problem sloved

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,24 @@
 # Lesson 40
+def factor(num):
+    result =[]
+    for i in range(1,num+1):
+        if num % i == 0:
+            result.append(i)
+    return result
+
+def prime(num):
+    return len(factor(num)) == 2
+
+def primelist(num):
+    primes = [2]
+    if num == 2:
+        return primes
+    else:
+        for i in range(3, num, 2):
+            if prime(i):
+                primes.append(i)
+
+        return primes
+
+# testing
+print(f'{primelist(100)}')
